3bb16960adafe0d88b44546b3fcc39ae315626fa
Hua Sun <33794673+hsun9@users.noreply.github.com>
2021-03-03 15:23:24 -0600

    Add files via upload

--- src/mergeMultipleFilesToOne.py
+++ src/mergeMultipleFilesToOne.py
@@ -1,9 +1,15 @@
 '''
 
     Hua Sun
+    2021-02-24 Added segment merging
     2021-01-30
 
-    Merage all of gene-level to one file
+    1.Merage all of segment-level to one file
+    2.Merage all of gene-level to one file
+    
+
+    // *.called.igv.seg
+    Sample  Chromosome      Start   End     Num_Probes      Call    Segment_Mean
     
     // *.geneLevel.from_seg.cn
     Sample   Chromosome      Start   End     Gene    Segment_Mean    Call
@@ -26,16 +32,31 @@ import pandas as pd
 
 
 dir = sys.argv[1]
-cmd = f'cat {dir}/*/*.geneLevel.from_seg.cn | grep -v Segment_Mean > {dir}/merged.geneLevel.cn.tmp'
+
+
+#----- 1.Merge segment
+cmd = f'cat {dir}/*/*.T.called.igv.seg | grep -v Segment_Mean > {dir}/merged.segment_level.cn.tmp'
 os.system(cmd)
 
-df = pd.read_csv(f'{dir}/merged.geneLevel.cn.tmp', sep='\t', header=None)
+df = pd.read_csv(f'{dir}/merged.segment_level.cn.tmp', sep='\t', header=None)
+df.columns = ['Sample','Chromosome','Start','End','Num_Probes','Call','Segment_Mean']
+df = df[['Sample','Chromosome','Start','End','Num_Probes','Segment_Mean','Call']]
+
+df.to_csv(f'{dir}/merged.segment_level.hg38.log2ratio.tsv', sep='\t', index=False)
+os.remove(f'{dir}/merged.segment_level.cn.tmp')
+
+
+
+#----- 2.Merge gene level
+cmd = f'cat {dir}/*/*.geneLevel.from_seg.cn | grep -v Segment_Mean > {dir}/merged.gene_level.cn.tmp'
+os.system(cmd)
+
+df = pd.read_csv(f'{dir}/merged.gene_level.cn.tmp', sep='\t', header=None)
 df.columns = ['Sample','Chromosome','Start','End','Gene','Segment_Mean','Call']
 
 df_spread = df.pivot_table(values='Segment_Mean', index=['Gene'], columns='Sample', aggfunc='first')
 df_spread = df_spread.reset_index()
 
-df_spread.to_csv(f'{dir}/merged.geneLevel.from_seg.hg38.log2ratio.tsv', sep='\t', index=False)
-os.remove(f'{dir}/merged.geneLevel.cn.tmp')
-
+df_spread.to_csv(f'{dir}/merged.gene_level.from_seg.hg38.log2ratio.tsv', sep='\t', index=False)
+os.remove(f'{dir}/merged.gene_level.cn.tmp')
 
